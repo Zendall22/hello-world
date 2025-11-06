fabcb947fd9154d8612ce5f212a39ed4f988c656
Zendall22 <hlanguqhawe@gmail.com>
2025-11-06 08:07:45 +0000

    question 2

--- solution.js
+++ solution.js
@@ -1,4 +1,11 @@
 // question 1
 let g = 8;
 let name =  'zobaphi';
-let ageknown = false
+let ageknown = false ;
+// question 2
+5 > 3 // true 5 is greater
+1 || -0 // 1 , one is the first truthy 
+8 && 0 // 0 , zero is the first falsey value
+3 == "5" // false , string is converted into number and 5 does not equal 3
+3 === "3" // false , string and number are different formats 
+alert(2 != 1) // true , the inverse of the result is given 
