600e9b099ed5e0e2e38634ef1726e795a27c5c27
Zendall22 <hlanguqhawe@gmail.com>
2025-11-06 08:17:29 +0000

    edited question 2 completed question 3

--- solution.js
+++ solution.js
@@ -3,9 +3,11 @@ let g = 8;
 let name =  'zobaphi';
 let ageknown = false ;
 // question 2
-5 > 3 // true 5 is greater
-1 || -0 // 1 , one is the first truthy 
-8 && 0 // 0 , zero is the first falsey value
-3 == "5" // false , string is converted into number and 5 does not equal 3
-3 === "3" // false , string and number are different formats 
-alert(2 != 1) // true , the inverse of the result is given 
+5 > 3;// true 5 is greater
+1 || -0 ; // 1 , one is the first truthy 
+8 && 0; // 0 , zero is the first falsey value
+3 == "5"; // false , string is converted into number and 5 does not equal 3
+3 === "3"; // false , string and number are different formats 
+alert(2 != 1); // true , the inverse of the result is given
+// question 3
+alert(`the result is '5 * 4 = 20`);
