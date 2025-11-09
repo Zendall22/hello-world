c4921b48ffe06798e0a406455e480233253ff990
Zendall22 <hlanguqhawe@gmail.com>
2025-11-09 09:00:18 +0000

    formatted the document

--- chatsolution.js
+++ chatsolution.js
@@ -1,5 +1,5 @@
 //question 1
-let name = "jessa"; 
+let name = "jessa";
 let age = 14;
 let employment = true; // you can change the variable you assign
 const stagnent = 7; // you cannot change the value of the constant
@@ -7,69 +7,69 @@ const stagnent = 7; // you cannot change the value of the constant
 let whoru = prompt('what is your name?',);
 let access = confirm('do you have access');
 if (confirm == 'OK') {
-    alert('welcome'); 
-}else {
+    alert('welcome');
+} else {
     alert('farewell');
 }
 // question 3
 5 === 5 // this is true 
 3 == '3' // this is true since it is not strict and it coerces the string to a number.
-(8 && 7 ) || !3   
+    (8 && 7) || !3
 // question 4
 let value = Number(prompt('provide a value',));
-if (value % 2 === 0 || value > 10 ) {
+if (value % 2 === 0 || value > 10) {
     alert('even and large');
 } else if (value % 2 === 0 || value < 10) {
     alert('even but small');
 } else alert('odd number');
 let result = alert(value);
 // question 5 
-for (let i = 0 ; i > 11 ; i ++) {
+for (let i = 0; i > 11; i++) {
     alert(i);
 }
 let z = 0;
 while (z > 11) {
-    alert(z) ;
-    z ++ ;
+    alert(z);
+    z++;
 }
-for (let f = 0 ; f > 11 ; f ++ ) {
-    if (f === 5) continue ;
+for (let f = 0; f > 11; f++) {
+    if (f === 5) continue;
     alert(f);
 }
 //question 5.4 ??
 // question 6
 switch (colour) {
     case 'red':
-        alert('bold') ;
-        break ;
+        alert('bold');
+        break;
     case 'blue':
-        alert('cool') ;
-        break ;
-    case 'yellow' :
+        alert('cool');
+        break;
+    case 'yellow':
         alert('bright');
-        break ;
-    case 'green' :
+        break;
+    case 'green':
         alert('fresh');
-        break ;
-    default :
-    alert('intresting choice');
-    
+        break;
+    default:
+        alert('intresting choice');
+
 }// not usually used in production code
 // question 7
 function checkNumber(value) {
- if (value > 0 ) {
-    return 'positive';
- } else if (value < 0) {
-    return 'negative';
- }else return 'zero';
+    if (value > 0) {
+        return 'positive';
+    } else if (value < 0) {
+        return 'negative';
+    } else return 'zero';
 }
 let checkNumberArrow = n => {
-   if (n > 0 ) {
-    return 'positive';
- } else if (n < 0) {
-    return 'negative';
- }else return 'zero'; 
-}  
+    if (n > 0) {
+        return 'positive';
+    } else if (n < 0) {
+        return 'negative';
+    } else return 'zero';
+}
 alert(checkNumberArrow)
 // question 8
 // it is important to put semicolons and use curly bracets around code to keep it neat and well formatted since code can spread over multiple lines and it is important that it easy to understand 
