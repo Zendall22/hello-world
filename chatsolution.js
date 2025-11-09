99c74842fcc0abf4e44e81e19f51607b03d2b091
Zendall22 <hlanguqhawe@gmail.com>
2025-11-09 03:54:31 +0000

    quetion 2

--- chatsolution.js
+++ chatsolution.js
@@ -3,3 +3,11 @@ let name = "jessa";
 let age = 14;
 let employment = true; // you can change the variable you assign
 const stagnent = 7; // you cannot change the value of the constant
+// question 2
+let whoru = prompt('what is your name?',);
+let access = confirm('do you have access');
+if (confirm == 'OK') {
+    alert('welcome'); 
+}else {
+    alert('farewell');
+}
